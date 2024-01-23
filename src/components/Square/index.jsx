f18bf65253b39ce63fad77d331c796280188dcf5
fpdellazari <fpdellazari@outlook.com>
2024-01-23 17:54:36 -0300

    Projeto adicionado

---
+++ src/components/Square/index.jsx
@@ -0,0 +1,10 @@
+import './styles.css';
+
+export default function Square({ value, onSquareClick }) {
+
+    return (
+        <>
+            <button className="square" onClick={onSquareClick}>{value}</button>
+        </>
+    );
+}
